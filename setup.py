285a89040db0a7b9fa80cffc64ea79fee5dcd8cd
erlapso <vbanfi@cyberwave.com>
2026-02-27 10:40:21 +0100

    fix dockerfile/2

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup
-import os
 from glob import glob
 
 package_name = 'mqtt_bridge'
@@ -7,10 +6,11 @@ package_name = 'mqtt_bridge'
 setup(
     name=package_name,
     version='0.1.0',
+    package_dir={'': 'mqtt_bridge'},
     packages=[package_name, package_name + '.plugins'],
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+            ['mqtt_bridge/resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/config',
             glob('mqtt_bridge/config/*.yaml')),
@@ -25,7 +25,6 @@ setup(
     maintainer_email='info@cyberwave.com',
     description='ROS2 MQTT bridge for Cyberwave digital twin platform',
     license='Apache-2.0',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'mqtt_bridge_node = mqtt_bridge.mqtt_bridge_node:main',
